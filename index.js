c5db70828d24d14cfc56c44c961fe918d472cf79
Danny Cardona <DLCardona@pm.me>
2020-06-26 19:06:59 -0500

    completed tasks 3 through 5 and started some of the CSS task

--- index.js
+++ index.js
@@ -224,19 +224,21 @@ artists[8].name = "Vincent Van Gogh";
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
-  }
-
+  return console.log("The artist at index " + array[index].id + " is " + array[index].name + ".")
+}
+getArtistByIndex(artists, 0);
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
-
-  /* Code here */
-
-}
+function get20s(array){
+let newArray = array.filter(function (e) {
+    return e.years.substring(0, 4) > "1899";
+});
+console.log(newArray)
+  }
+  get20s(artists);
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
@@ -249,12 +251,12 @@ function get20s(/* Code here */){
  *
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.
 */
-function removeArtist(/*code here*/) {
-    /* code here */
-  }
+function removeArtist(array, index) {
+  array.splice(index, 1);
+  return array;
 
-
-
+}
+console.log(removeArtist(artists, 0));
 /**
 /* Task 6: Create a function called `addArtist` that can accept an object of information and add it to the artists array. Then, add a 21st artist to the array (you) with custom information!👩‍🎨👨‍🎨
 
